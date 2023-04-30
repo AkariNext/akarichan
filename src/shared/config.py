a172330a7f1e7dae304b6bbb260ae222d84d4872
yupix <yupi0982@outlook.jp>
2023-04-30 21:17:43 +0900

    feat: guild_idsをconfigに追加

--- src/shared/config.py
+++ src/shared/config.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 
 class Config:
     def __init__(self) -> None:
@@ -11,6 +12,11 @@ class Config:
         if len(token) == 0:
             raise Exception('tokenが設定されていません。config.iniを見直してください')
         return token
+    
+    @property
+    def guild_ids(self) -> list[int]:
+        guild_ids: str = self._config.get('BOT', 'guild_ids')
+        return json.loads(guild_ids)
 
 
 akarichan_config = Config()

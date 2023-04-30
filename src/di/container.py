18c99403253e079c24da959792c39a4c0242313d
yupix <yupi0982@outlook.jp>
2023-04-30 21:11:35 +0900

    chore: ディレクトリ名の変更

--- src/di/container.py
+++ src/di/container.py
@@ -1,6 +1,6 @@
 from injector import Injector
 
-from src.di_container.modules.user_module import UserModule
+from src.di.modules.user_module import UserModule
 
 di_container = Injector([UserModule])
 
